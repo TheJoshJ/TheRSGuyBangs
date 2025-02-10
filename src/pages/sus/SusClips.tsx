ccef3c90acddf8d1a9715e3caa23ee906234ea3b
Josh Jones <thejoshjones1@gmail.com>
2025-02-10 13:59:13 -0800

    This might fix it?

--- src/pages/sus/SusClips.tsx
+++ src/pages/sus/SusClips.tsx
@@ -3,7 +3,7 @@ const SusClips = () => {
     <div className="flex flex-col items-center gap-5">
       <div>
         <iframe
-          src="https://clips.twitch.tv/embed?clip=ElegantPoisedPotKlappa-5v0HhIT2lHjoIK0-&parent=www.thersguybangs.com"
+          src="https://clips.twitch.tv/embed?clip=ElegantPoisedPotKlappa-5v0HhIT2lHjoIK0-&parent=https://thersguybangs.com/"
           height="350"
           width="620"
         ></iframe>
